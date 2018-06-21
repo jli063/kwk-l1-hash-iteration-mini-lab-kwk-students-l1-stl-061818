48f9df1b80d68925199d9619344c7f101a913fe7
jli063 <jli063@rsdmo.org>
2018-06-21 17:58:13 +0000

    automatically backed up by learn

--- zodiac_project.rb
+++ zodiac_project.rb
@@ -5,7 +5,7 @@ c.burger
 d.salad "
 
 puts "What do you like to do on a Friday night?"
-puts "a. hang out with friends
+puts "a. go to a party
 b. stay at home
 c. go to a party
 d. hang out with friends"

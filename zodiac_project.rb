bb7624c4377a0ff8962990e1ad202664732724b8
jli063 <jli063@rsdmo.org>
2018-06-21 17:53:15 +0000

    automatically backed up by learn

--- zodiac_project.rb
+++ zodiac_project.rb
@@ -1,10 +1,10 @@
 puts "What is your favorite food?"
-puts "A.sushi  
-B.fruit
-C.burger
-D.salad "
+puts "a.sushi  
+b.fruit
+c.burger
+d.salad "
 
 puts "What do you like to do on a Friday night?"
-puts "A. go to a party
-B. watch TV at home
-C. "
+puts "a. go to a party
+b. watch TV at home
+c.  "

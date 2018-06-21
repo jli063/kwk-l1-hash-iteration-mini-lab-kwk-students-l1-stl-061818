fb67e6004769c41f6712f170638c251f4db1f34f
jli063 <jli063@rsdmo.org>
2018-06-21 18:17:11 +0000

    automatically backed up by learn

--- zodiac_project.rb
+++ zodiac_project.rb
@@ -7,6 +7,14 @@
   c.burger
   d.salad "
   food=gets.chomp
+  if food=="a"
+    total=total+1
+  if food=="b"
+    total=total+2
+  if food=="c"
+    total=total+3
+  if food=="d"
+    total=total+4
 
 
 # puts "What is your favorite hobby?"
